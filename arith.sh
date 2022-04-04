d79029f94104970419452dcfd70aeaf4dc69d063
urmila4462 <dasari.urmila4462@gmail.com>
2022-04-04 23:25:07 +0530

    [urmila]ADDED implemented arith result in arithmetic computing

--- arith.sh
+++ arith.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 read -p "Enter 3 inputs: " a b c
 echo "Three inputs are:a=$a b=$b c=$c"
@@ -19,6 +19,13 @@ echo "value of compute3 is:"$z
 p=$(($a%$b+$c))
 echo "value of compute4 is:"$p
 
+#store result
+declare -a Result
+Result[0]=$x
+Result[1]=$y
+Result[2]=$z
+Result[3]=$p
+echo "The Results of every Computation in Dictionary :" ${Result[@]}
 
 
 
